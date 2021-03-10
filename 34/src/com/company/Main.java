ba667c4da76573a2c5d2ee8a7d3c9f0297bf782d
danil1nanatolyi <80408487+danil1nanatolyi@users.noreply.github.com>
2021-03-10 19:00:01 +0300

    Add files via upload

---
+++ 34/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println("input a");
+        int b = in.nextInt();
+        System.out.println("input b");
+        int c = in.nextInt();
+        System.out.println("input b");
+        int x = in.nextInt();
+        System.out.println("input x");
+        int f=in.nextInt();
+        int f=a*(-1)*Math.pow(x,2)-b;
+        if(x + 5 < 0 & c = 0) {
+        int f = (x - a) / (x);
+            if (x > 5 & c != 0) {
+             int f = x * (-1) / c;
+                System.out.println(f);
+            }
+        }
+    }
+}
